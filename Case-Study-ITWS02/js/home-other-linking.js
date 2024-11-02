3f54bb7ecae29f25e22e6d016975677f0d42ef66
RzCs <168328462+RzCs1@users.noreply.github.com>
2024-11-02 17:59:41 +0800

    Delete Case-Study-ITWS02 directory

--- Case-Study-ITWS02/js/home-other-linking.js
+++
@@ -1,9 +0,0 @@
-const home_cover_links = {
-  news: 'html-pages/news.html',
-  about: 'html-pages/about.html'
-};
-
-document.querySelector("#home-cover-buttons").innerHTML = `
-  <a href="${home_cover_links.news}" class="buttonlink">News</a>
-  <a href="${home_cover_links.about}" class="buttonlink">About us</a>
-`;
